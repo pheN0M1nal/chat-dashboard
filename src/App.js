e77519eec16a0e636a9b3279d86666123ee3676b
pheN0M1nal <nmnashrf@gmail.com>
2021-10-12 11:32:03 +0500

    v-1.01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import logoimg from './img/logo.jpeg';
 
 function App() {
-  return (
-    <div className="App">
-      Heyy
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='sideNav'>
+				<div className='wrapper'>
+					<div className='userLogo'>
+						<img
+							className='img'
+							src={logoimg}
+							alt='user logo'
+						/>
+					</div>
+					<div className='userName'>user name</div>
+					<div className='navItem'>navItem</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
